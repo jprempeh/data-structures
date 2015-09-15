e398cb7e1f79909b8e75723b1b864bfc476c5b49
Joe Prempeh <prempehjoe@gmail.com>
2015-09-15 15:14:20 -0700

    functional shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,40 @@
 var Queue = function(){
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+	var instance = {};
+		  instance.storage = {};
+			instance.start = -1;
+			instance.end = -1;
+	extend(instance, queueMethods);
+
+	return instance;
+};
+
+var extend = function(to, from) {
+	for(var key in from) {
+		to[key] = from[key];
+	}
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(string) {
+	this.end++;
+	this.storage[this.end] = string;
+};
 
+queueMethods.dequeue = function() {
+	this.size() && this.start++;
+	var temp = this.storage[this.start];
+	delete this.storage[this.start];
+	return temp;
+};
+
+queueMethods.size = function() {
+	return (this.end - this.start);
+};
+
+var list = Queue();
+list.enqueue('foo');
+console.log(list.sum())
+console.log(list);
+list.dequeue();
 
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -8,12 +8,13 @@ var Queue = function(){
 		instance.size() && start++;
 		var result = storage[start];
 		delete storage[start];
-		return result;
+		return result || 'the queue is empty!';
 	};
 
 	instance.enqueue = function(string) {
 		end++;
 		storage[end] = string;
+		return string;
 	};
 
 	instance.size = function() {
@@ -22,3 +23,20 @@ var Queue = function(){
 
 	return instance;
 };
+
+var myHoes = new Queue();
+
+console.log(myHoes.enqueue('Aui'));
+console.log(myHoes.enqueue('Fon'));
+console.log(myHoes.enqueue('Hwawa'));
+console.log(myHoes.enqueue('Dew'));
+console.log(myHoes.enqueue('Rain'));
+console.log(myHoes.size());
+console.log(myHoes.dequeue());
+console.log(myHoes.dequeue());
+console.log(myHoes.dequeue());
+console.log(myHoes.dequeue());
+console.log(myHoes.dequeue());
+console.log(myHoes.dequeue());
+console.log(myHoes.size());
+console.log(myHoes);
